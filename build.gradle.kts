bc71736c56c68942147c622111edb8082ad1e586
Riad Gozim <rgozim1@gmail.com>
2019-03-20 17:57:55 +0000

    adding build config generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin"))
+    implementation("com.squareup:javapoet:1.11.1")
     implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.9.3")
     implementation("org.ajoberstar:grgit:1.9.1") {
         setForce(true)
---
+++ src/main/kotlin/org/openmicroscopy/tasks/ClassField.kt
@@ -0,0 +1,4 @@
+package org.openmicroscopy.tasks
+
+class ClassField {
+}
