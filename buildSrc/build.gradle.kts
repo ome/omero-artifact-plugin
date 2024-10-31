24f0ed862e97dafd50355b99ab03403839f79296
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-10-31 15:46:02 +0000

    Remove guava

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin"))
     implementation("org.jfrog.buildinfo:build-info-extractor-gradle:5.2.5")
+    implementation("org.ajoberstar.grgit:grgit-core:5.3.0") 
     implementation("org.ajoberstar.grgit:grgit-gradle:5.3.0") 
     implementation("org.ajoberstar.git-publish:gradle-git-publish:4.2.2")
 }
